92a7d0d32e78324e219c7f3b7b7669a5d998a0d7
hassan80testaoy <107244855+hassan80testaoy@users.noreply.github.com>
2023-01-07 14:01:08 +0200

    Update plugins.js

--- plugins.js
+++ plugins.js
@@ -1,4 +1,53 @@
-/*<![CDATA[*/ function darkMode(){localStorage.setItem("mode","darkmode"===localStorage.getItem("mode")?"light":"darkmode"),"darkmode"===localStorage.getItem("mode")?document.querySelector("#mainCont").classList.add("drK"):document.querySelector("#mainCont").classList.remove("drK")}; function headScroll() {const distanceY = window.pageYOffset || document.documentElement.scrollTop, shrinkOn = 40, commentEl = document.getElementById('header');if (distanceY > shrinkOn) {commentEl.classList.add("stick");} else {commentEl.classList.remove("stick");} } window.addEventListener('scroll', headScroll); 
-/* lazy youtube */ ( function() {var youtube = document.querySelectorAll(".lazyYt"); for (var i = 0; i < youtube.length; i++) {var source = "https://img.youtube.com/vi/"+ youtube[i].dataset.embed +"/sddefault.jpg"; var image = new Image(); image.setAttribute("class", "lazy"); image.setAttribute("data-src",source); image.setAttribute("src","data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="); image.setAttribute("alt","Youtube video"); image.addEventListener( "load", function() {youtube[ i ].appendChild( image );}( i ) ); youtube[i].addEventListener( "click", function() {var iframe = document.createElement( "iframe" ); iframe.setAttribute( "frameborder", "0" ); iframe.setAttribute( "allowfullscreen", "" ); iframe.setAttribute( "src", "https://www.youtube.com/embed/"+ this.dataset.embed +"?rel=0&showinfo=0&autoplay=1" ); this.innerHTML = ""; this.appendChild( iframe ); }); }; })();
-/* Lightbox image script, source: kompiajaib.com/2021/09/update-image-lightbox-dengan-css-dan.html */ for (var imageslazy = document.querySelectorAll('.pS .separator img, .pS .tr-caption-container img, .pS .psImg >img, .pS .btImg >img'), i = 0; i < imageslazy.length; i++) imageslazy[i].setAttribute('onclick', 'return false'); function wrap(o, t, e) {for (var i = document.querySelectorAll(t), c = 0; c < i.length; c++) {var a = o + i[c].outerHTML + e; i[c].outerHTML = a} } wrap('<div class="zmImg">', '.pS .separator >a', '</div>'); wrap('<div class="zmImg">', '.pS .tr-caption-container td >a', '</div>'); wrap('<div class="zmImg">', '.pS .separator >img', '</div>'); wrap('<div class="zmImg">', '.pS .tr-caption-container td >img', '</div>'); wrap('<div class="zmImg">', '.pS .psImg >img', '</div>'); wrap('<div class="zmImg">', '.pS .btImg >img', '</div>'); for (var containerimg = document.getElementsByClassName('zmImg'), i = 0; i < containerimg.length; i++) containerimg[i].onclick = function() {this.classList.toggle('s');};
-Defer.dom('.lazy', 100, 'loaded', null, {rootMargin:'1px'}),'undefined'!=typeof infinite_scroll&&infinite_scroll.on('load',function(){Defer.dom('.lazy', 100, 'loaded', null, {rootMargin:'1px'}) }); /*]]>*/
+<script>
+<b:if cond='data:view.isPost'>
+    //<![CDATA[
+$(document).ready(function(){$(tocList).empty();var t=null,a=null,e=0;$(".amp-content h2, .amp-content h3").each(function(){var n="<a name='"+e+"'></a>";$(this).before(n);var o="<li><a href='#"+e+"'>"+$(this).text()+"</a></li>";$(this).is("h2 , h3")?(a=$("<ul></ul>"),(t=$(o)).append(a),t.appendTo("#tocList")):a.append(o),e++})});
+if (document['getElementById']('a3lan-title')) {
+    document['getElementsByClassName']('a3lan-title')[0]['innerHTML'] = document['getElementById']('a3lan-title')['innerHTML'];
+    document['getElementById']('a3lan-title')['remove']()
+};
+if (document['getElementById']('a3lan-first')) {
+    document['getElementsByClassName']('a3lan-first')[0]['innerHTML'] = document['getElementById']('a3lan-first')['innerHTML'];
+    document['getElementById']('a3lan-first')['remove']()
+};
+if (document['getElementById']('a3lan-Center')) {
+    document['getElementsByClassName']('a3lan-Center')[0]['innerHTML'] = document['getElementById']('a3lan-Center')['innerHTML'];
+    document['getElementById']('a3lan-Center')['remove']()
+};
+if (document['getElementById']('a3lan-End')) {
+    document['getElementsByClassName']('a3lan-End')[0]['innerHTML'] = document['getElementById']('a3lan-End')['innerHTML'];
+    document['getElementById']('a3lan-End')['remove']()
+};
+if (document['getElementById']('a3lan-lates')) {
+    document['getElementsByClassName']('a3lan-lates')[0]['innerHTML'] = document['getElementById']('a3lan-lates')['innerHTML'];
+    document['getElementById']('a3lan-lates')['remove']()
+};
+
+    //]]>
+</b:if>
+<b:if cond='not data:view.isSingleItem'>
+    //<![CDATA[
+var olderLink = $('a.blog-pager-older-link')['attr']('href');
+olderLink && $('.loadMorePosts')['show'](), $('.loadMorePosts a')['on']('click', function(_0xe79bx3) {
+    $('.loadMorePosts')['hide'](), $['ajax']({
+        url: olderLink,
+        success: function(_0xe79bx3) {
+            var _0xe79bx4 = $(_0xe79bx3)['find']('.blog-posts');
+            _0xe79bx4['children']('.status-msg-wrap')['remove'](), $('.blog-posts')['append'](_0xe79bx4['html']()), (olderLink = $(_0xe79bx3)['find']('.blog-pager-older-link')['attr']('href')) ? $('.loadMorePosts')['show']() : $('.noMorePosts')['show']()
+        },
+        beforeSend: function() {
+            $('.loadMore > #loader')['show']()
+        },
+        complete: function() {
+            $('.loadMore > #loader')['hide']()
+        }
+    }), _0xe79bx3['preventDefault']()
+});
+ 
+//]]>
+</b:if>
+    //<![CDATA[
+$(document).ready(function(){$('#ScrollTop').click(function() {$('html, body').animate({scrollTop: 0}, 800); return false;});});
+cookieChoices = {};
+    //]]>
+</script>
