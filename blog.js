8b9a7ce1d34a27c75f56b67dec17ddb2eb8f1dbb
hassan80testaoy <107244855+hassan80testaoy@users.noreply.github.com>
2023-10-03 16:22:48 +0300

    Update blog.js

--- blog.js
+++ blog.js
@@ -20,7 +20,7 @@ if (document['getElementById']('a3lan-lates')) {
 };
 $(document).ready(function() {
   setTimeout(function() {
-    $('head').append('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/fontawesome.min.css">');
+    $('head').append('<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/fontawesome.min.css">');
   }, 3000);
 });
 var lazyadsense=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyadsense||0!=document.body.scrollTop&&!1===lazyadsense)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyadsense=!0)},!0);
